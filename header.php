f1824056194c9b927d6ab71693ee1ac5f35227e3
JOW <john.geo16@gmail.com>
2017-10-23 15:40:30 +0200

    Entfernt js Datein zur auto-Vervollständigung

--- header.php
+++ header.php
@@ -56,9 +56,6 @@ else {
     <title>Digitales Museum Startseite</title>
     <link rel="stylesheet" href="css/bootstrap-3.3.7-dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="css/style.css">
-    <script type="text/javascript" src="js/jquery-1.9.1.min.js"></script>
-    <script type="text/javascript" src="js/jquery.autocomplete.min.js"></script>
-    <script type="text/javascript" src="js/currency-autocomplete.js"></script>
 </head>
 
 <body onload="mail();">
